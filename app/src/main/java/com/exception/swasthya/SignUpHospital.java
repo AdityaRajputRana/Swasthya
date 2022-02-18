94a79edae24525dc95683520538af2bc50f16e4f
abhaysood1793 <abhayasood99@gmail.com>
2022-02-19 00:17:04 +0530

    signup ui completed, firebase not connecting

--- app/src/main/java/com/exception/swasthya/SignUpHospital.java
+++ app/src/main/java/com/exception/swasthya/SignUpHospital.java
@@ -3,12 +3,30 @@ package com.exception.swasthya;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.EditText;
 
 public class SignUpHospital extends AppCompatActivity {
+
+    EditText editTextName;
+    EditText editTextPhone;
+    EditText editTextEmail;
+    EditText editTextAadhar;
+    EditText editTextpassword;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign_up_hospital);
+
+        initUI();
+    }
+
+    public void initUI(){
+        editTextAadhar = (EditText) findViewById(R.id.editText_aadhar);
+        editTextPhone = (EditText) findViewById(R.id.editText_phone);
+        editTextEmail = (EditText) findViewById(R.id.editText_email);
+        editTextName = (EditText) findViewById(R.id.editText_name);
+        editTextpassword = (EditText) findViewById(R.id.editText_pswd);
+
     }
 }

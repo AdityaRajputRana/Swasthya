4d1b13f8e1811a7c231a8fb775b5a9e92dd38bdc
abhaysood1793 <abhayasood99@gmail.com>
2022-02-19 01:15:26 +0530

    minorChanges

--- app/src/main/java/com/exception/swasthya/MainActivity.java
+++ app/src/main/java/com/exception/swasthya/MainActivity.java
@@ -2,6 +2,7 @@ package com.exception.swasthya;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
@@ -11,6 +12,7 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-
+        Intent intent = new Intent(MainActivity.this,SignUpHospital.class);
+        startActivity(intent);
     }
 }

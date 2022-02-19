231ed5995332f2014e817592daef5f6985e04d74
Aditya Rana <adityarajputrana2016@gmail.com>
2022-02-20 01:17:17 +0530

    Location Fetching is now available

--- app/src/main/java/com/exception/swasthya/MainActivity.java
+++ app/src/main/java/com/exception/swasthya/MainActivity.java
@@ -2,15 +2,25 @@ package com.exception.swasthya;
 
 
 import androidx.activity.result.ActivityResultLauncher;
+import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
 import androidx.core.content.ContextCompat;
 
 import android.Manifest;
 import android.content.Intent;
 import android.content.pm.PackageManager;
+import android.location.Location;
 import android.os.Build;
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.Toast;
+
+import com.google.android.gms.location.FusedLocationProviderClient;
+import com.google.android.gms.location.LocationServices;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 
@@ -27,29 +37,75 @@ public class MainActivity extends AppCompatActivity {
 
     }
 
-   public void checkPermissions(){
-       if (ContextCompat.checkSelfPermission(
-               getApplicationContext(), Manifest.permission.INTERNET) ==
-               PackageManager.PERMISSION_GRANTED) {
-           mAuth = FirebaseAuth.getInstance();
-           FirebaseUser currentUser = mAuth.getCurrentUser();
-           if(currentUser == null){
-               Intent intent = new Intent(MainActivity.this, SignUpActivity.class);
-               startActivity(intent);
-               this.finish();
-           } else {
-               setContentView(R.layout.activity_main);
-               startMain();
-           }
-       } else {
-           if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
-               requestPermissions(new String[]{Manifest.permission.INTERNET}, 1001);
-           }
-       }
-   }
+    public void checkPermissions() {
+        if (ContextCompat.checkSelfPermission(
+                getApplicationContext(), Manifest.permission.INTERNET) ==
+                PackageManager.PERMISSION_GRANTED
+                &&
+                ContextCompat.checkSelfPermission(
+                        getApplicationContext(), Manifest.permission.ACCESS_FINE_LOCATION) ==
+                        PackageManager.PERMISSION_GRANTED) {
+            mAuth = FirebaseAuth.getInstance();
+            FirebaseUser currentUser = mAuth.getCurrentUser();
+            if (currentUser == null) {
+                Intent intent = new Intent(MainActivity.this, SignUpActivity.class);
+                startActivity(intent);
+                this.finish();
+            } else {
+                setContentView(R.layout.activity_main);
+                startMain();
+            }
+        } else {
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+                requestPermissions(new String[]{Manifest.permission.INTERNET, Manifest.permission.ACCESS_FINE_LOCATION}, 1001);
+            }
+        }
+    }
+
+    private FusedLocationProviderClient fusedLocationClient;
 
     private void startMain() {
+        /*How we are going to find
+       get current location
+       covert to geohash
+       search geo hash
+       display
+       display on map
+       diplay a slider for different hospitals
+        * */
 
+        getLastLocation();
+    }
+
+    private void getLastLocation() {
+        fusedLocationClient = LocationServices.getFusedLocationProviderClient(this);
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED ) {
+            checkPermissions();
+            return;
+        }
+
+        fusedLocationClient.getLastLocation()
+                .addOnFailureListener(new OnFailureListener() {
+                    @Override
+                    public void onFailure(@NonNull Exception e) {
+                        Log.i("SWA", "Failed"+ e.getMessage());
+                    }
+                })
+                .addOnSuccessListener(this, new OnSuccessListener<Location>() {
+                    @Override
+                    public void onSuccess(Location location) {
+                        Log.i("SWA", "succ");
+
+                        if (location != null) {
+                            fetchHospitals(location);
+                        }
+                    }
+                });
+    }
+
+    private void fetchHospitals(Location location) {
+        Toast.makeText(this, "Location Fetched", Toast.LENGTH_SHORT).show();
+        Log.i("SWA", "Location");
     }
 
     @Override

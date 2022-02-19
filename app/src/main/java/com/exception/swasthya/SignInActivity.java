51046f225ba490ae7925606bb215f5d099d11ab5
abhaysood1793 <abhayasood99@gmail.com>
2022-02-19 22:13:10 +0530

    basic login fuctoinality

---
+++ app/src/main/java/com/exception/swasthya/SignInActivity.java
@@ -0,0 +1,70 @@
+package com.exception.swasthya;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
+public class SignInActivity extends AppCompatActivity {
+
+
+
+    private FirebaseAuth mAuth;
+
+    Button buttonSignIn;
+    EditText editTestEmailOnSignIn;
+    EditText editTestPasswordOnSignIn;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sign_in);
+
+        buttonSignIn = (Button)findViewById(R.id.button_sign_in_onsignup);
+        editTestEmailOnSignIn = (EditText) findViewById(R.id.editText_user_name_sign_in);
+        editTestPasswordOnSignIn = (EditText) findViewById(R.id.editText_user_email_sign_in);
+
+
+        buttonSignIn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String email = editTestEmailOnSignIn.getText().toString();
+                String password = editTestPasswordOnSignIn.getText().toString();
+
+                mAuth.signInWithEmailAndPassword(email, password)
+                        .addOnCompleteListener(SignInActivity.this, new OnCompleteListener<AuthResult>() {
+                            @Override
+                            public void onComplete(@NonNull Task<AuthResult> task) {
+                                if (task.isSuccessful()) {
+                                    // Sign in success, update UI with the signed-in user's information
+                                    //Log.d(TAG, "signInWithEmail:success");
+                                    FirebaseUser user = mAuth.getCurrentUser();
+                                    Toast.makeText(SignInActivity.this, "Sign In Successful",
+                                            Toast.LENGTH_SHORT).show();
+                                    finish();
+                                } else {
+                                    // If sign in fails, display a message to the user.
+                                    //Log.w(TAG, "signInWithEmail:failure", task.getException());
+                                    Toast.makeText(SignInActivity.this, "Authentication failed. Please Retry",
+                                            Toast.LENGTH_SHORT).show();
+
+                                }
+                            }
+                        });
+
+            }
+        });
+    }
+}
--- app/src/main/java/com/exception/swasthya/SignUpActivity.java
+++ app/src/main/java/com/exception/swasthya/SignUpActivity.java
@@ -1,9 +1,21 @@
 package com.exception.swasthya;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 public class SignUpActivity extends AppCompatActivity {
 
@@ -12,6 +24,15 @@ public class SignUpActivity extends AppCompatActivity {
     EditText editTextEmail;
     EditText editTextAadhar;
     EditText editTextpassword;
+    Button button;
+    Button buttonSignInInstead;
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+    }
+
+    private FirebaseAuth mAuth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,14 +40,58 @@ public class SignUpActivity extends AppCompatActivity {
         setContentView(R.layout.activity_sign_up);
 
         initUI();
+        mAuth = FirebaseAuth.getInstance();
+
+
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                String email = editTextEmail.getText().toString();
+                String password = editTextpassword.getText().toString();
+
+
+                mAuth.signInWithEmailAndPassword(email, password)
+                        .addOnCompleteListener(SignUpActivity.this, new OnCompleteListener<AuthResult>() {
+                            @Override
+                            public void onComplete(@NonNull Task<AuthResult> task) {
+                                if (task.isSuccessful()) {
+                                    // Sign in success, update UI with the signed-in user's information
+                                    Toast.makeText(SignUpActivity.this, "Sign up Successful",
+                                            Toast.LENGTH_SHORT).show();
+                                    finish();
+                                } else {
+                                    // If sign in fails, display a message to the user.
+                                    //Log.w(TAG, "signInWithEmail:failure", task.getException());
+                                    Toast.makeText(SignUpActivity.this, "Authentication failed. Please Retry",
+                                            Toast.LENGTH_SHORT).show();
+
+
+                                }
+                            }
+                        });
+
+            }
+        });
+
+        buttonSignInInstead.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(SignUpActivity.this, SignInActivity.class);
+                startActivity(intent);
+            }
+        });
+
     }
 
-    public void initUI(){
+    public void initUI() {
         editTextAadhar = (EditText) findViewById(R.id.editText_user_aadhar);
         editTextPhone = (EditText) findViewById(R.id.editText_user_phone);
         editTextEmail = (EditText) findViewById(R.id.editText_user_email);
         editTextName = (EditText) findViewById(R.id.editText_user_name);
         editTextpassword = (EditText) findViewById(R.id.editText_user_pswd);
+        button = (Button) findViewById(R.id.button_create_account);
+        buttonSignInInstead = (Button) findViewById(R.id.button_sign_in_onsignup);
 
     }
 }

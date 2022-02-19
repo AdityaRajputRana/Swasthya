9ac98f6a3de7e3267bea9aa635bc49b9752b7aa6
Aditya Rana <adityarajputrana2016@gmail.com>
2022-02-19 22:10:50 +0530

    Sign ups linked and emergency sign in is working

--- app/src/main/java/com/exception/swasthya/MainActivity.java
+++ app/src/main/java/com/exception/swasthya/MainActivity.java
@@ -5,14 +5,24 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 
+import com.google.firebase.auth.FirebaseAuth;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
 
-        Intent intent = new Intent(MainActivity.this,SignUpHospital.class);
-        startActivity(intent);
+        FirebaseAuth mAuth = FirebaseAuth.getInstance();
+        if (mAuth.getCurrentUser() == null){
+            Intent intent = new Intent(MainActivity.this,SignUpActivity.class);
+            startActivity(intent);
+        } else {
+            //This is where main activity is loaded.
+            setContentView(R.layout.activity_main);
+
+        }
+
+
     }
 }
--- app/src/main/java/com/exception/swasthya/SignUpActivity.java
+++ app/src/main/java/com/exception/swasthya/SignUpActivity.java
@@ -1,9 +1,18 @@
 package com.exception.swasthya;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
 
 public class SignUpActivity extends AppCompatActivity {
 
@@ -19,11 +28,46 @@ public class SignUpActivity extends AppCompatActivity {
         setContentView(R.layout.activity_sign_up);
 
         initUI();
+        setUpListeners();
+    }
+
+    private void setUpListeners() {
+        findViewById(R.id.btnHospital)
+                .setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View view) {
+                        Intent intent = new Intent(SignUpActivity.this, SignUpHospital.class);
+                        startActivity(intent);
+                        SignUpActivity.this.finish();
+                    }
+                });
+
+        findViewById(R.id.btnEmergency)
+                .setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View view) {
+                        createAnonymousUser();
+                    }
+                });
+    }
+
+    private void createAnonymousUser() {
+        FirebaseAuth auth = FirebaseAuth.getInstance();
+        auth.signInAnonymously()
+                .addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()){
+                            startActivity(new Intent(SignUpActivity.this, MainActivity.class));
+                            SignUpActivity.this.finish();
+                        } else {
+                            Toast.makeText(SignUpActivity.this, "Some error occurred: "+ task.getException().getMessage(), Toast.LENGTH_SHORT).show();
+                        }
+                    }
+                });
     }
 
     public void initUI(){
-        editTextAadhar = (EditText) findViewById(R.id.editText_user_aadhar);
-        editTextPhone = (EditText) findViewById(R.id.editText_user_phone);
         editTextEmail = (EditText) findViewById(R.id.editText_user_email);
         editTextName = (EditText) findViewById(R.id.editText_user_name);
         editTextpassword = (EditText) findViewById(R.id.editText_user_pswd);
